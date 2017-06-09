29804d3a5f21adacfc97bc7301a6785c2d90ed1e
Matthew Lean <matt@mattlean.com>
2017-06-09 02:39:24 -0700

    Finish basic image upload example

--- script.js
+++ script.js
@@ -40,13 +40,38 @@ var testForm = document.getElementById('test_form');
 testForm.addEventListener('submit', function(evt) {
   evt.preventDefault();
 
-  var formData = {
-    c_test_str: document.getElementById('c_test_str').value
-  };
+  var formData = {};
 
-  console.log('fileData: ', fileData);
+  var c_test_str = document.getElementById('c_test_str').value;
+  if(c_test_str) {
+    formData.c_test_str = c_test_str;
+  }
+
+  if(fileData.file && fileData.fileURL) {
+    formData.c_image = { content: fileData.file.name };
+  }
 
   cortexAPI.request('POST', 'c_tests', formData, function(responseText) {
-    console.log('Cortex API Request Complete: ', JSON.parse(responseText));
+    var response = JSON.parse(responseText);
+    console.log('Cortex API Request Complete: ', response);
+
+    if(fileData.file && fileData.fileURL) {
+      var imageXHR = new XMLHttpRequest();
+      imageXHR.open('POST', response.c_image.uploads[0].uploadUrl);
+
+      var imageForm = new FormData();
+
+      for(var i in response.c_image.uploads[0].fields) {
+        var currField = response.c_image.uploads[0].fields[i];
+        imageForm.append(currField.key, currField.value);
+      }
+
+      imageForm.append('file', fileData.file);
+
+      imageXHR.send(imageForm);
+
+      fileData.file = null;
+      fileData.fileURL = null;
+    }
   });
 });
